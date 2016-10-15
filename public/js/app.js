2b3755b495870bd9b4c9a5f45620a439f1960a78
om13shukla <omshuklait@gmail.com>
2016-10-15 18:22:38 -0500

    Correction in Http call

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope, $http) {
     $scope.callServer=function() {
-                $http.get("http://localhost:3000/jsonfiles/chartData.json")
+                $http.get("http://cse5335-ors1582.herokuapp.com/jsonfiles/chartData.json")
                         .then(function(response) {
                 $scope.msg=response.data;
                 //console.log(response);
