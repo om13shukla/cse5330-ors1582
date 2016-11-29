78b38544bc2ce0a9db3d9dc07e438d83f3db623e
om13shukla <omshuklait@gmail.com>
2016-11-28 20:02:30 -0600

    working fixes-10.7.5 find query result from  M-LAB

--- public/js/app.js
+++ public/js/app.js
@@ -93,9 +93,9 @@ app.controller('myCtrl4', function($scope, $http) {
             $http.post('http://cse5335-ors1582.herokuapp.com/api/emps/findOne',fparam).then(handleSuccess, handleError);
     }
      function handleSuccess(res) {
-            console.log( res.data);
+            console.log( res);
             
-            $scope.premsg41 = res;
+            $scope.premsg41 = res.data;
         }
 
         function handleError(res) {
