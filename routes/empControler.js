98f8ee40e7e7df8bebb190e34d381a8e4ca1b84c
om13shukla <omshuklait@gmail.com>
2016-11-28 19:36:03 -0600

    working fixes-10.6.8 find query result from  M-LAB

--- routes/empControler.js
+++ routes/empControler.js
@@ -44,7 +44,7 @@ function findOne(req, res) {
         });
     
     */
-      console.log('FORM EMPCONTROLLER' emp);
+      console.log('FORM EMPCONTROLLER'+ emp);
       if(emp){res.send(emp);}
     
 }
